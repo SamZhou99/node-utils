9ed311984f12e6e31a294626b5fca21605eee9d3
SamZhou99 <45636987@qq.com>
2019-12-03 15:54:31 +0800

    update

--- test.js
+++ test.js
@@ -11,14 +11,14 @@ const db01 = new mysqlSync({
     port: '3306',
     user: 'root',
     password: 'root',
-    database: 'lajiao_video'
+    database: 'test01'
 });
 const db02 = new mysqlSync({
     host: '127.0.0.1',
     port: '3306',
     user: 'root',
     password: 'root',
-    database: 'jimo100'
+    database: 'test02'
 });
 
 let test = {
@@ -28,21 +28,21 @@ let test = {
         await this.testDB(db01);
         await this.testDB(db02);
         await test.checkUrl();
+        // 参数说明
+        // new DownLoadClass(downloadUrl, savePath, value, callback, isDebug);
         new DownloadBigFile('https://w.wallhaven.cc/full/ey/wallhaven-eyo1xk.jpg?1', './', {}, (err, res) => {
             console.log(err, res);
         }, true);
-        // new DownLoadClass(downloadUrl, savePath, value, callback, isDebug);
-
     },
     async testDB(db) {
         let result = await db.Query('SELECT * FROM articles LIMIT ?', [10]);
         console.log(result[0].title, result[0].a_title);
     },
     async checkUrl() {
-        let result = await checkUrl.http('https://www.tianshiw.club/?m=vod-play-id-6238-src-1-num-1.html');
+        let result = await checkUrl.http('http://tool.chinaz.com/');
         console.log('http模块测试结果：', result);
 
-        result = await checkUrl.axios('https://www.tianshiw.club');
+        result = await checkUrl.axios('http://www.chinaz.com/');
         console.log('axios模块测试结果：', result);
     },
 };
